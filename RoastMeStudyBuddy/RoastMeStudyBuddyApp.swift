874598c504129588dfc6af8bf6a04825266273ad
rohinqaderyan <ahmad.qaderyan@pfizer.com>
2026-02-15 23:46:29 -0500

    Initial Commit

---
+++ RoastMeStudyBuddy/RoastMeStudyBuddyApp.swift
@@ -0,0 +1,32 @@
+//
+//  RoastMeStudyBuddyApp.swift
+//  RoastMeStudyBuddy
+//
+//  Created by Qaderyan, Rohin on 2/15/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RoastMeStudyBuddyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ RoastMeStudyBuddyTests/RoastMeStudyBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoastMeStudyBuddyTests.swift
+//  RoastMeStudyBuddyTests
+//
+//  Created by Qaderyan, Rohin on 2/15/26.
+//
+
+import Testing
+@testable import RoastMeStudyBuddy
+
+struct RoastMeStudyBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
